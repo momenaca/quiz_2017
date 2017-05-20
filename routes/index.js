dce6e14ca73c22f2290d53dc2bfbb2cbda5b62fa
Miguel Omeñaca <omenacamiguel@gmail.com>
2017-05-20 18:40:55 +0200

    Despliegue Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); // Un router de express es un MW que permite agrupar otros MWs de atención a rutas
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { title: 'Quiz' });
 });
 
 // Pagina de creditos
 router.get('/author', function(req, res, next) {
     res.render('author');
+});
+
+//Pagina de ayuda
+router.get('/help', function(req, res, next) {
+    res.render('help');
 });
 
 module.exports = router;
